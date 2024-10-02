8efd064367b418b0220e952e0423e79c02b4073d
florianbgt <florian.bigot321@gmail.com>
2024-10-02 10:36:35 +0200

    bootstrap repo

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+);
