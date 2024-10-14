25b41e067fcca95aa3b2389afc3dec11c51ae24c
florianbgt <florian.bigot321@gmail.com>
2024-10-14 12:16:23 +0200

    bootstrap API endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,50 +1,42 @@
 import { Router } from "express";
-import path from "path";
 
 const api = Router();
 
-// Annotate an api
-api.post("/annotate", (req, res) => {
-  console.log("Annotating");
-  res.send("Annotating");
+api.post("/annotate/openai", (req, res) => {
+  const body: {
+    entrypointPath: string;
+    targetDir?: string;
+    apiKey: string;
+  } = req.body;
+
+  res.status(200).send("Annotating");
 });
 
-// TODO return the list of API endpoints from an entrypoint
-// [
-//   {
-//     "method": "GET",
-//     "path": "/scan",
-//     "dependencies": [
-//       "/index.ts",
-//       "router.ts",
-//       "/utils.ts",
-//     ],
-//   },
-// ]
-api.post("/scan", (req, res) => {
+api.post("/api/scan", (req, res) => {
   const body: {
     entrypointPath: string;
     targetDir?: string;
   } = req.body;
 
-  res.json([
-    {
-      method: "GET",
-      path: "/scan",
-      dependencies: ["/index.ts", "router.ts", "/utils.ts"],
-    },
-    {
-      method: "POST",
-      path: "/scan",
-      dependencies: ["/index.ts", "router.ts", "/utils.ts"],
-    },
-  ]);
+  res.status(200).json([]);
 });
 
-// TODO split the codebase into multiple ones according to annotation
-api.post("/split", (req, res) => {
-  console.log("Splitting");
-  res.send("Splitting");
+api.post("/api/group", (req, res) => {
+  const body: {
+    entrypointPath: string;
+    targetDir?: string;
+    groups: { name: string; endpoints: { method: string; path: string }[] }[];
+  } = req.body;
+
+  res.status(200);
+});
+
+api.post("/api/split", (req, res) => {
+  const body: {
+    entrypointPath: string;
+    targetDir?: string;
+  } = req.body;
+  res.status(200).json([]);
 });
 
 export default api;
