8307dcf510d0587ea22c742fe9051fd355aac684
Florian <florian.bigot321@gmail.com>
2024-10-24 11:32:52 +0200

    remove warning in production (#5)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,6 +10,13 @@ import { findAvailablePort, openInBrowser } from "./helper/server";
 import initCommandHandler from "./commands/init";
 import { getConfigFromWorkDir, getOpenaiApiKeyFromConfig } from "./config";
 
+// remove all warning.
+// We need this because of some depreciation warning we have with 3rd party libraries
+// Only on production so we still have them on devlopment
+if (process.env.NODE_ENV !== "development") {
+  process.removeAllListeners("warning");
+}
+
 yargs(hideBin(process.argv))
   // Global options, used for all commands
   .options({
