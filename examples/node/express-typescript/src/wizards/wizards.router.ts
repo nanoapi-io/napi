04981eb26e95770e7c39a6691ff20a6e134901be
Florian <florian.bigot321@gmail.com>
2024-12-17 15:46:02 +0000

    improve annotate-openai (#55)
    * improve annotate-openai
    
    * fix

--- examples/node/express-typescript/src/wizards/wizards.router.ts
+++ examples/node/express-typescript/src/wizards/wizards.router.ts
@@ -4,20 +4,20 @@ import * as wizardsService from "./wizards.service";
 
 const router = Router();
 
-// @nanoapi path:/api/v0/wizards/ method:GET
+// @nanoapi method:GET path:/api/v0/wizards/
 router.get("/", async (_req, res) => {
   const wizards = await wizardsService.findAll();
   res.send(wizards);
 });
 
-// @nanoapi path:/api/v0/wizards/:id method:GET
+// @nanoapi method:GET path:/api/v0/wizards/:id
 router.get("/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const wizard = await wizardsService.findById(id);
   res.send(wizard);
 });
 
-// @nanoapi path:/api/v0/wizards/ method:POST
+// @nanoapi method:POST path:/api/v0/wizards/
 router.post("/", async (req, res) => {
   const wizard = req.body;
   const newWizard = await wizardsService.create(wizard);
--- packages/cli/src/commands/annotate.ts
+++ packages/cli/src/commands/annotate.ts
@@ -1,91 +1,118 @@
 import fs from "fs";
 import DependencyTreeManager from "../dependencyManager/dependencyManager";
 import OpenAI from "openai";
-import { DependencyTree } from "../dependencyManager/types";
+import { getLanguagePlugin } from "../languagesPlugins";
+import { File } from "../splitRunner/types";
+import { removeIndexesFromSourceCode } from "../helper/file";
+import prompts from "prompts";
+
+function removeAllAnnotations(entryPointPath: string, files: File[]) {
+  const updatedFiles = files.map((file) => {
+    const plugin = getLanguagePlugin(entryPointPath, file.path);
+
+    const tree = plugin.parser.parse(file.sourceCode);
+
+    const annotationsNodes = plugin.getAnnotationNodes(tree.rootNode);
+
+    const indexesToRemove = annotationsNodes.map((node) => {
+      return {
+        startIndex: node.startIndex,
+        endIndex: node.endIndex,
+      };
+    });
+
+    const newSourceCode = removeIndexesFromSourceCode(
+      file.sourceCode,
+      indexesToRemove,
+    );
+
+    return {
+      ...file,
+      sourceCode: newSourceCode,
+    };
+  });
+
+  return updatedFiles;
+}
 
 export default async function annotateOpenAICommandHandler(
   entrypoint: string, // Path to the entrypoint file
   openAIApiKey: string, // OpenAI API key
 ) {
+  const response = await prompts({
+    type: "confirm",
+    name: "confirm",
+    message: `This process will remove all existing annotations and generate new ones. Do you want to proceed?`,
+    initial: false,
+  });
+  if (!response.confirm) {
+    return;
+  }
+
   console.info("Annotating program...");
   const dependencyTreeManager = new DependencyTreeManager(entrypoint);
+
+  let files = dependencyTreeManager.getFiles();
+
+  files = removeAllAnnotations(entrypoint, files);
 
   const openAIConfig = {
     apiKey: openAIApiKey,
   };
   const openAIClient = new OpenAI(openAIConfig);
 
-  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
-    {
-      role: "system",
-      content: `You are a helpful assistant that provides detailed annotations for API endpoints.`,
-    },
-  ];
-  messages.push({
-    role: "user",
-    content: `The entrypoint of my application is: ${entrypoint}.
-    Here is the dependency tree of the entrypoint of: ${JSON.stringify(dependencyTreeManager.dependencyTree)}`,
-  });
-
-  messages.push({
-    role: "user",
-    content: `I am now going to walkthrough the files one by one and provide the content of each file.
-    I want you to understand what the application is doing and where each endpoint is.`,
-  });
-
-  function iterateOverTree(tree: DependencyTree) {
-    messages.push({
-      role: "user",
-      content: `File: ${tree.path}
-      Content: ${tree.sourceCode}`,
-    });
-
-    tree.children.forEach((child) => {
-      messages.push({
-        role: "user",
-        content: `File: ${child.path}
-        Content: ${child.sourceCode}`,
-      });
-
-      iterateOverTree(child);
-    });
-  }
-  iterateOverTree(dependencyTreeManager.dependencyTree);
-
-  messages.push({
-    role: "user",
-    content: `I want you to add the following annotation above each function that handle an API call (as a comment):
-    @nanoapi method:GET path:/\\/api\\/endpoint/
-    You need to replace "GET" with the actual HTTP method.
-    You need to replace "/\\/api\\/endpoint/" with a REGEX that match the actual endpoint path (full path).
-    You are forbidden from changing the code. You can only add comments.
-    If there is existing annotations, you need to replace them. If there is not, you need to add them
-    I want your response to be in the following JSON format:
-    [
-      {
-        "file": "path/to/file",
-        "content": "content of the file with annotations"
-      }
-    ]
-    Provide only a parsable JSON output as your response. Without any additional text or markdown.
-    I will parse your response directly as JSON without any manipulation.`,
-  });
+  const filesContent = JSON.stringify(
+    files.map((file) => {
+      return {
+        path: file.path,
+        content: file.sourceCode,
+      };
+    }),
+    null,
+    2,
+  );
 
   const chatCompletion = await openAIClient.chat.completions.create({
-    messages: messages,
-    model: "gpt-4-turbo",
+    model: "o1-preview",
+    messages: [
+      {
+        role: "user",
+        content: `
+        Add annotations as a comment above each block of code that handle an API call.
+        Do it only for where the API endpoints are registered (final endpoint, but also any middleware that handle the request).
+        You are not allowed to modify the code or add comments other than the annotations.
+        The annotations should be in the following format (as a comment, use the correct comment syntax for the language of the file):
+        @nanoapi method:GET path:api/v1/endpoint
+        Replace "method" with CRUD operation used in the endpoint. If there is no method then omit it.
+        Replace "path" by the full path of the API. Dynamic parts of the path should be in the following format: "path:api/v1/<id>".
+        Respond in the same format as the user input. With no markdown formatting.
+        `,
+      },
+      {
+        role: "user",
+        content: filesContent,
+      },
+    ],
   });
-
   const responseContent = chatCompletion.choices[0].message.content as string;
 
-  const annotatedContents: { file: string; content: string }[] =
-    JSON.parse(responseContent);
+  const annotatedContent: {
+    path: string;
+    content: string;
+  }[] = JSON.parse(responseContent);
 
-  for (const annotatedContent of annotatedContents) {
-    const filePath = annotatedContent.file;
-    const content = annotatedContent.content;
-    fs.writeFileSync(filePath, content);
-  }
+  annotatedContent.forEach((annotatedFile) => {
+    const file = files.find((file) => file.path === annotatedFile.path);
+    if (!file) {
+      throw new Error(`File ${annotatedFile.path} not found`);
+    }
+
+    file.sourceCode = annotatedFile.content;
+  });
+
+  files.forEach((file) => {
+    fs.writeFileSync(file.path, file.sourceCode, "utf-8");
+  });
 
   console.info(
     "Annotation complete. OpenAI can make mistakes, so please review the annotations.",
