f95cc04c5f435c9db6e22964a1f336d243b382b0
joeldevelops <joeldevelops@nanoapi.io>
2025-03-12 11:42:26 +0100

    Fix welcome modal for dark mode

--- packages/app/src/components/WelcomeModal.tsx
+++ packages/app/src/components/WelcomeModal.tsx
@@ -1,7 +1,4 @@
-import {
-  Button,
-  Dialog
-} from "@radix-ui/themes";
+import { Button, Dialog } from "@radix-ui/themes";
 import { useState, useEffect } from "react";
 
 export default function WelcomeModal(props: {
@@ -26,23 +23,58 @@ export default function WelcomeModal(props: {
         <Dialog.Title>
           <div className="flex justify-end">
             <Dialog.Close>
-              <Button variant="ghost" className="hover:bg-transparent cursor-pointer">
-                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M18.7071 7.20711C19.0976 6.81658 19.0976 6.18342 18.7071 5.79289C18.3166 5.40237 17.6834 5.40237 17.2929 5.79289L12 11.0858L6.70711 5.79289C6.31658 5.40237 5.68342 5.40237 5.29289 5.79289C4.90237 6.18342 4.90237 6.81658 5.29289 7.20711L10.5858 12.5L5.29289 17.7929C4.90237 18.1834 4.90237 18.8166 5.29289 19.2071C5.68342 19.5976 6.31658 19.5976 6.70711 19.2071L12 13.9142L17.2929 19.2071C17.6834 19.5976 18.3166 19.5976 18.7071 19.2071C19.0976 18.8166 19.0976 18.1834 18.7071 17.7929L13.4142 12.5L18.7071 7.20711Z" fill="currentColor"/>
+              <Button
+                variant="ghost"
+                className="hover:bg-transparent cursor-pointer"
+              >
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M18.7071 7.20711C19.0976 6.81658 19.0976 6.18342 18.7071 5.79289C18.3166 5.40237 17.6834 5.40237 17.2929 5.79289L12 11.0858L6.70711 5.79289C6.31658 5.40237 5.68342 5.40237 5.29289 5.79289C4.90237 6.18342 4.90237 6.81658 5.29289 7.20711L10.5858 12.5L5.29289 17.7929C4.90237 18.1834 4.90237 18.8166 5.29289 19.2071C5.68342 19.5976 6.31658 19.5976 6.70711 19.2071L12 13.9142L17.2929 19.2071C17.6834 19.5976 18.3166 19.5976 18.7071 19.2071C19.0976 18.8166 19.0976 18.1834 18.7071 17.7929L13.4142 12.5L18.7071 7.20711Z"
+                    fill="currentColor"
+                  />
                 </svg>
               </Button>
             </Dialog.Close>
           </div>
         </Dialog.Title>
         <h1 className="text-3xl font-bold text-center p-4">👋 Hey there!</h1>
-        <p className="text-xl text- text-center pb-5">Welcome to the NanoAPI live demo</p>
-        <p className="text-lg text-text-gray text-center pb-5 px-5">This is a live demo prefilled with <b>static</b> data from one of our customer's projects, available for you to view and inspect to see how our tool works.</p> 
-        <p className="text-lg text-text-gray text-center pb-5 px-5">You may notice some functionality is disabled, which is intentional for the purpose of keeping this demo clean and clear.</p>
-        <p className="text-lg text-text-gray text-center pb-5 px-5">If you'd like to see these views with your own projects, please reach out to our team to schedule a personalized demo at: <a href="mailto:info@nanoapi.io?subject=Request for Live Demo" target="_blank" className="text-primary-light dark:text-primary-dark underline">info@nanoapi.io</a></p>
+        <p className="text-xl text- text-center pb-5">
+          Welcome to the NanoAPI live demo
+        </p>
+        <p className="text-lg text-text-gray dark:text-white text-center pb-5 px-5">
+          This is a live demo prefilled with <b>static</b> data from one of our
+          customer's projects, available for you to view and inspect to see how
+          our tool works.
+        </p>
+        <p className="text-lg text-text-gray dark:text-white text-center pb-5 px-5">
+          You may notice some functionality is disabled, which is intentional
+          for the purpose of keeping this demo clean and clear.
+        </p>
+        <p className="text-lg text-text-gray dark:text-white text-center pb-5 px-5">
+          If you'd like to see these views with your own projects, please reach
+          out to our team to schedule a personalized demo at:{" "}
+          <a
+            href="mailto:info@nanoapi.io?subject=Request for Live Demo"
+            target="_blank"
+            className="text-primary-light dark:text-primary-dark underline"
+          >
+            info@nanoapi.io
+          </a>
+        </p>
         <div className="w-full flex justify-center">
-          <Button type="submit" 
-            onClick={() => props?.setIsOpen ? props?.setIsOpen(false) : setOpen(false)}
-            className="mt-5 text-lg w-full font-bold bg-primary-light dark:bg-primary-dark hover:bg-primary-hoverLight dark:hover:bg-primary-hoverDark rounded-lg cursor-pointer px-6 py-5 transition-all">
+          <Button
+            type="submit"
+            onClick={() =>
+              props?.setIsOpen ? props?.setIsOpen(false) : setOpen(false)
+            }
+            className="mt-5 text-lg w-full font-bold bg-primary-light dark:bg-primary-dark hover:bg-primary-hoverLight dark:hover:bg-primary-hoverDark rounded-lg cursor-pointer px-6 py-5 transition-all"
+          >
             Continue
           </Button>
         </div>
