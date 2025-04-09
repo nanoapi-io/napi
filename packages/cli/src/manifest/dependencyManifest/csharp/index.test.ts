2b36b125ff5df0461bcc72f590172d932dba44bd
LeBoufty <boufto@gmail.com>
2025-04-09 11:38:41 +0200

    Update manifest tests

--- packages/cli/src/manifest/dependencyManifest/csharp/index.test.ts
+++ packages/cli/src/manifest/dependencyManifest/csharp/index.test.ts
@@ -9,79 +9,76 @@ import {
 describe("generateCSharpDependencymanifest", () => {
   const files = getCSharpFilesMap();
   const manifest = generateCSharpDependencyManifest(files);
+  const burgers = path.join(csharpFilesFolder, "2Namespaces1File.cs");
+  const models = path.join(csharpFilesFolder, "Models.cs");
+  const namespaced = path.join(csharpFilesFolder, "Namespaced.cs");
+  const nested = path.join(csharpFilesFolder, "Nested.cs");
+  const program = path.join(csharpFilesFolder, "Program.cs");
+  const semiNamespaced = path.join(csharpFilesFolder, "SemiNamespaced.cs");
+  const usage = path.join(csharpFilesFolder, "Usage.cs");
+
   test("Correctly identifies files", () => {
     expect(Object.keys(manifest).length).toBe(9);
   });
+
   test("Resolves exports", () => {
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "2Namespaces1File.cs")].symbols,
-      ).length,
-    ).toBe(6);
-    expect(
-      Object.keys(manifest[path.join(csharpFilesFolder, "Models.cs")].symbols)
-        .length,
-    ).toBe(5);
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "Namespaced.cs")].symbols,
-      ).length,
-    ).toBe(1);
-    expect(
-      Object.keys(manifest[path.join(csharpFilesFolder, "Nested.cs")].symbols)
-        .length,
-    ).toBe(3);
-    expect(
-      Object.keys(manifest[path.join(csharpFilesFolder, "Program.cs")].symbols)
-        .length,
-    ).toBe(1);
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "SemiNamespaced.cs")].symbols,
-      ).length,
-    ).toBe(3);
-    expect(
-      Object.keys(manifest[path.join(csharpFilesFolder, "Usage.cs")].symbols)
-        .length,
-    ).toBe(1);
+    expect(Object.keys(manifest[burgers].symbols).length).toBe(6);
+    expect(Object.keys(manifest[models].symbols).length).toBe(5);
+    expect(Object.keys(manifest[namespaced].symbols).length).toBe(1);
+    expect(Object.keys(manifest[nested].symbols).length).toBe(3);
+    expect(Object.keys(manifest[program].symbols).length).toBe(1);
+    expect(Object.keys(manifest[semiNamespaced].symbols).length).toBe(3);
+    expect(Object.keys(manifest[usage].symbols).length).toBe(1);
   });
+
   test("Resolves dependencies", () => {
+    expect(Object.keys(manifest[burgers].dependencies).length).toBe(1);
+    expect(Object.keys(manifest[models].dependencies).length).toBe(1);
+    expect(Object.keys(manifest[namespaced].dependencies).length).toBe(1);
+    expect(Object.keys(manifest[nested].dependencies).length).toBe(1);
+    expect(Object.keys(manifest[program].dependencies).length).toBe(6);
+    expect(Object.keys(manifest[semiNamespaced].dependencies).length).toBe(2);
+    expect(Object.keys(manifest[usage].dependencies).length).toBe(4);
+  });
+
+  test("Resolves dependents", () => {
+    expect(
+      Object.keys(manifest[burgers].symbols["MyApp.BeefBurger.Bun"].dependents)
+        .length,
+    ).toBe(1);
+    expect(
+      Object.keys(manifest[burgers].symbols["ChickenBurger.Bun"].dependents)
+        .length,
+    ).toBe(1);
     expect(
       Object.keys(
-        manifest[path.join(csharpFilesFolder, "2Namespaces1File.cs")]
-          .dependencies,
+        manifest[namespaced].symbols["MyNamespace.MyClass"].dependents,
       ).length,
     ).toBe(1);
     expect(
       Object.keys(
-        manifest[path.join(csharpFilesFolder, "Models.cs")].dependencies,
+        manifest[semiNamespaced].symbols["HalfNamespace.Gordon"].dependents,
       ).length,
     ).toBe(1);
     expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "Namespaced.cs")].dependencies,
-      ).length,
-    ).toBe(1);
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "Nested.cs")].dependencies,
-      ).length,
-    ).toBe(1);
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "Program.cs")].dependencies,
-      ).length,
-    ).toBe(6);
-    expect(
-      Object.keys(
-        manifest[path.join(csharpFilesFolder, "SemiNamespaced.cs")]
-          .dependencies,
-      ).length,
+      Object.keys(manifest[semiNamespaced].symbols["Freeman"].dependents)
+        .length,
     ).toBe(2);
     expect(
       Object.keys(
-        manifest[path.join(csharpFilesFolder, "Usage.cs")].dependencies,
+        manifest[nested].symbols["OuterNamespace.OuterInnerClass"].dependents,
       ).length,
-    ).toBe(4);
+    ).toBe(1);
+    expect(
+      Object.keys(
+        manifest[nested].symbols["OuterNamespace.InnerNamespace.InnerClass"]
+          .dependents,
+      ).length,
+    ).toBe(1);
+    expect(
+      Object.keys(
+        manifest[models].symbols["MyApp.Models.OrderStatus"].dependents,
+      ).length,
+    ).toBe(1);
   });
 });
