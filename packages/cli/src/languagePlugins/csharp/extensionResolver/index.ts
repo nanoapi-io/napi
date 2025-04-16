4bb7b9ed97d225d3da455b67bc51a6204e0f372f
LeBoufty <boufto@gmail.com>
2025-04-16 11:11:56 +0200

    Extension resolver

---
+++ packages/cli/src/languagePlugins/csharp/extensionResolver/index.ts
@@ -0,0 +1,113 @@
+import {
+  SymbolNode,
+  NamespaceNode,
+  CSharpNamespaceMapper,
+} from "../namespaceMapper";
+import { csharpParser } from "../../../helpers/treeSitter/parsers";
+import Parser from "tree-sitter";
+
+const extensionMethodQuery = new Parser.Query(
+  csharpParser.getLanguage(),
+  `
+  ((method_declaration
+    parameters : (parameter_list
+    (parameter
+      (modifier) @mod)
+    ))
+  (#eq? @mod "this")) @method
+`,
+);
+
+/**
+ * Interface representing an extension method in C#.
+ */
+export interface ExtensionMethod {
+  /**
+   * The syntax node of the extension method.
+   */
+  node: Parser.SyntaxNode;
+  /**
+   * The symbol associated with the extension method.
+   */
+  symbol: SymbolNode;
+  /**
+   * The name of the extension method.
+   */
+  name: string;
+}
+
+/**
+ * Record for the extensions present in a namespace.
+ * Key : name of the method.
+ * Value : ExtensionMethod object of the method.
+ */
+export type NamespaceExtensions = Record<string, ExtensionMethod>;
+/**
+ * Record for all extensions in the project.
+ * Key : name of a namespace that has extensions.
+ * Value : the extensions of said namespace.
+ */
+export type ExtensionMethodMap = Record<string, NamespaceExtensions>;
+
+export class CSharpExtensionResolver {
+  private namespaceMapper: CSharpNamespaceMapper;
+
+  constructor(namespaceMapper: CSharpNamespaceMapper) {
+    this.namespaceMapper = namespaceMapper;
+  }
+
+  /**
+   * Resolves extension methods in a symbol.
+   * @param symbol - the symbol to analyse.
+   * @returns A map of extension methods found in the file.
+   */
+  public resolveExtensionMethods(symbol: SymbolNode): NamespaceExtensions {
+    const extensions: NamespaceExtensions = {};
+    const extensionMethods = extensionMethodQuery.captures(symbol.node);
+    for (const ext of extensionMethods) {
+      if (ext.name === "mod") continue;
+      const methodNode = ext.node;
+      const methodName = methodNode.childForFieldName("name")?.text;
+      if (methodName) {
+        extensions[methodName] = {
+          node: methodNode,
+          symbol: symbol,
+          name: methodName,
+        };
+      }
+    }
+    return extensions;
+  }
+
+  public resolveExtensionMethodsInNamespace(
+    namespace: NamespaceNode,
+  ): NamespaceExtensions {
+    const extensions: NamespaceExtensions = {};
+    for (const symbol of namespace.exports) {
+      const extMethods = this.resolveExtensionMethods(symbol);
+      Object.assign(extensions, extMethods);
+    }
+    return extensions;
+  }
+
+  /**
+   * Resolves extension methods in the namespace tree.
+   * @returns A map of extension methods found in the project.
+   */
+  public resolveExtensionMethodsInNamespaceTree(): ExtensionMethodMap {
+    const extensions: ExtensionMethodMap = {};
+
+    const resolveExtensionsRecursively = (namespace: NamespaceNode) => {
+      const extMethods = this.resolveExtensionMethodsInNamespace(namespace);
+      if (Object.keys(extMethods).length > 0) {
+        extensions[this.namespaceMapper.getFullNSName(namespace)] = extMethods;
+      }
+      for (const childNamespace of namespace.childrenNamespaces) {
+        resolveExtensionsRecursively(childNamespace);
+      }
+    };
+
+    resolveExtensionsRecursively(this.namespaceMapper.nsTree);
+    return extensions;
+  }
+}
