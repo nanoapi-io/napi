21720a9815cd1c45d488748189e51988a3ee1259
LeBoufty <boufto@gmail.com>
2025-04-16 17:33:08 +0200

    Fixed tests

--- packages/cli/src/languagePlugins/csharp/extensionResolver/index.test.ts
+++ packages/cli/src/languagePlugins/csharp/extensionResolver/index.test.ts
@@ -12,7 +12,7 @@ describe("CSharpExtensionResolver", () => {
   test("should resolve extension methods in the project", () => {
     expect(Object.keys(extensions).length).toBe(1);
     expect(extensions[""]).toBeDefined();
-    expect(extensions[""]["Bite"]).toBeDefined();
-    expect(extensions[""]["Heal"].name).toBeDefined();
+    expect(extensions[""].length).toBe(2);
+    expect(extensions[""][0].name).toBeDefined();
   });
 });
