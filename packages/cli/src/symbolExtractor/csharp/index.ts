8ee632d856063c27726cb0c8d4b18d1ea6c4307d
LeBoufty <boufto@gmail.com>
2025-04-28 16:26:29 +0200

    Removed obsolete usings

--- packages/cli/src/symbolExtractor/csharp/index.ts
+++ packages/cli/src/symbolExtractor/csharp/index.ts
@@ -8,6 +8,15 @@ import {
 } from "../../languagePlugins/csharp/extractor/index.js";
 import { DependencyManifest } from "@nanoapi.io/shared";
 import { DotNetProject } from "../../languagePlugins/csharp/projectMapper/index.js";
+import Parser from "tree-sitter";
+import { csharpParser } from "../../helpers/treeSitter/parsers.js";
+import { CSharpNamespaceMapper } from "../../languagePlugins/csharp/namespaceMapper/index.js";
+import { CSharpProjectMapper } from "../../languagePlugins/csharp/projectMapper/index.js";
+import {
+  CSharpUsingResolver,
+  InternalSymbol,
+  ExternalSymbol,
+} from "../../languagePlugins/csharp/usingResolver/index.js";
 
 /**
  * Extracts C# symbols from the given files.
@@ -27,6 +36,11 @@ export function extractCSharpSymbols(
   console.time(`Extracted ${symbolsToExtract.size} symbol(s)`);
   const extractor = new CSharpExtractor(files, dependencyManifest);
   const extractedFiles: ExtractedFile[] = [];
+  const parsedFiles = new Map<
+    string,
+    { path: string; rootNode: Parser.SyntaxNode }
+  >();
+  const csprojFiles = new Map<string, { path: string; content: string }>();
   // Extract symbols from the files
   for (const symbolSet of symbolsToExtract.values()) {
     for (const symbol of symbolSet.symbols) {
@@ -62,9 +76,16 @@ export function extractCSharpSymbols(
       `${name}.cs`,
     );
     if (!extractedFilesMap.has(key)) {
+      const filecontent = extractor.getContent(extractedFile);
+      // Add the extracted file to the output map
       extractedFilesMap.set(key, {
         path: key,
-        content: extractor.getContent(extractedFile),
+        content: filecontent,
+      });
+      // Also add the file to a separate project for using resolution.
+      parsedFiles.set(key, {
+        path: key,
+        rootNode: csharpParser.parse(filecontent).rootNode,
       });
     }
   }
@@ -73,6 +94,7 @@ export function extractCSharpSymbols(
     const projectPath = path.join(subproject.name, `${subproject.name}.csproj`);
     const globalUsingPath = path.join(subproject.name, "GlobalUsings.cs");
     if (!extractedFilesMap.has(projectPath)) {
+      // Add the project files to the output map
       extractedFilesMap.set(projectPath, {
         path: projectPath,
         content: subproject.csprojContent,
@@ -81,6 +103,37 @@ export function extractCSharpSymbols(
         path: globalUsingPath,
         content: extractor.generateGlobalUsings(subproject),
       });
+      // Add the project files to the parsed files map for using resolution
+      csprojFiles.set(projectPath, {
+        path: projectPath,
+        content: subproject.csprojContent,
+      });
+      parsedFiles.set(globalUsingPath, {
+        path: globalUsingPath,
+        rootNode: csharpParser.parse(extractor.generateGlobalUsings(subproject))
+          .rootNode,
+      });
+    }
+  }
+  // For each extracted file, check if the using directives are still valid
+  const nsMapper = new CSharpNamespaceMapper(parsedFiles);
+  const pjMapper = new CSharpProjectMapper(csprojFiles);
+  const usingResolver = new CSharpUsingResolver(nsMapper, pjMapper);
+  for (const extractedFile of extractedFiles) {
+    const imports = extractedFile.imports;
+    for (const importDirective of imports) {
+      const resolvedInNewFile =
+        usingResolver.resolveUsingDirective(importDirective);
+      const resolvedInOldFile =
+        extractor.usingResolver.resolveUsingDirective(importDirective);
+      if (
+        resolvedInNewFile instanceof ExternalSymbol &&
+        resolvedInOldFile instanceof InternalSymbol
+      ) {
+        extractedFile.imports = extractedFile.imports.filter(
+          (imp) => imp !== importDirective,
+        );
+      }
     }
   }
   console.timeEnd(`Extracted ${symbolsToExtract.size} symbol(s)`);
