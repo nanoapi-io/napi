c1faf2a6850d4c865c3d7443955935e4c7d2873f
LeBoufty <boufto@gmail.com>
2025-05-01 14:39:42 +0200

    Added C parser

--- packages/cli/src/helpers/treeSitter/parsers.ts
+++ packages/cli/src/helpers/treeSitter/parsers.ts
@@ -1,6 +1,7 @@
 import Parser, { Language } from "tree-sitter";
 import Python from "tree-sitter-python";
 import CSharp from "tree-sitter-c-sharp";
+import C from "tree-sitter-c";
 
 const pythonParser = new Parser();
 pythonParser.setLanguage(Python as Language);
@@ -9,5 +10,9 @@ const pythonLanguage = Python.name as "python";
 const csharpParser = new Parser();
 csharpParser.setLanguage(CSharp as Language);
 const csharpLanguage = CSharp.name as "c-sharp";
+
+const cParser = new Parser();
+cParser.setLanguage(C as Language);
+const cLanguage = C.name as "c";
 
 export { pythonParser, pythonLanguage, csharpParser, csharpLanguage };
