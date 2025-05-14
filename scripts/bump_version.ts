3b15e7c5b209c6230c127c3094c8a8fc42646007
florianbgt <florian.bigot321@gmail.com>
2025-05-14 11:11:12 +0200

    attempt to fix release workflow

--- scripts/bump_version.ts
+++ scripts/bump_version.ts
@@ -8,11 +8,6 @@ if (
   console.error("Invalid release type");
   Deno.exit(1);
 }
-const releaseNotes: string = Deno.args[1];
-if (!releaseNotes) {
-  console.error("Release notes are required");
-  Deno.exit(1);
-}
 
 const denoJsonPath = path.resolve(
   import.meta.dirname as string,
@@ -40,23 +35,6 @@ denoJson.version = newVersion;
 await Deno.writeTextFile(
   denoJsonPath,
   JSON.stringify(denoJson, null, 2) + "\n",
-);
-
-const changeLogPath = path.resolve(
-  import.meta.dirname as string,
-  "..",
-  "CHANGELOG.md",
-);
-const currentChangeLog = await Deno.readTextFile(changeLogPath);
-
-const newContent =
-  `## [${newVersion}] - ${new Date().toISOString().split("T")[0]}` + "\n\n" +
-  releaseNotes + "\n";
-const allContent = newContent + "\n" + currentChangeLog;
-
-await Deno.writeTextFile(
-  changeLogPath,
-  allContent,
 );
 
 console.log(newVersion);
