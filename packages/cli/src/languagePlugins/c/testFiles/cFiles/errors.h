39b9eb3f71f7827348e96fba200642031a694126
LeBoufty <boufto@gmail.com>
2025-05-21 13:28:50 +0200

    New test file

---
+++ packages/cli/src/languagePlugins/c/testFiles/cFiles/errors.h
@@ -0,0 +1,13 @@
+// Unnamed enum
+enum {
+    ONE, TWO, THREE, VIVA, L, ALGERIE
+};
+
+// Non-standard typedef
+struct Salut_t {
+	int id;
+    int waow;
+} typedef Salut;
+
+// Syntax error
+int x =??;
