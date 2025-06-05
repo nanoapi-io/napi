a451e440495f9a2ebcf43810751824193cd48e54
LeBoufty <boufto@gmail.com>
2025-06-05 11:40:15 +0200

    Changed file structure in mapper

--- packages/cli/src/languagePlugins/java/packageMapper/index.ts
+++ packages/cli/src/languagePlugins/java/packageMapper/index.ts
@@ -1,19 +1,22 @@
 import { JavaTree } from "./types.ts";
 import type Parser from "tree-sitter";
 import { JavaPackageResolver } from "../packageResolver/index.ts";
+import type { JavaFile } from "../packageResolver/types.ts";
 
 export class JavaPackageMapper {
   tree: JavaTree;
-  files: Map<string, { path: string; rootNode: Parser.SyntaxNode }>;
+  files: Map<string, JavaFile>;
 
   constructor(
     files: Map<string, { path: string; rootNode: Parser.SyntaxNode }>,
   ) {
-    this.files = files;
+    this.files = new Map();
     this.tree = new JavaTree();
     const resolver = new JavaPackageResolver();
     for (const f of files.values()) {
-      this.tree.addFile(resolver.resolveFile(f));
+      const resolvedFile = resolver.resolveFile(f);
+      this.tree.addFile(resolvedFile);
+      this.files.set(f.path, resolvedFile);
     }
   }
 }
